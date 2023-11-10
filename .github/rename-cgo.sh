d8b6524d455ecd8c407a0fce853ae9fc046f876e
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2023-11-10 16:42:50 +0800

    Update rename-cgo.sh

--- .github/rename-cgo.sh
+++ .github/rename-cgo.sh
@@ -5,25 +5,25 @@ for FILENAME in $FILENAMES
 do
     if [[ $FILENAME =~ "darwin-10.16-arm64" ]];then
         echo "rename darwin-10.16-arm64 $FILENAME"
-        mv $FILENAME clash.meta-darwin-arm64-cgo
+        mv $FILENAME lucky.core-darwin-arm64-cgo
     elif [[ $FILENAME =~ "darwin-10.16-amd64" ]];then
         echo "rename darwin-10.16-amd64 $FILENAME"
-        mv $FILENAME clash.meta-darwin-amd64-cgo
+        mv $FILENAME lucky.core-darwin-amd64-cgo
     elif [[ $FILENAME =~ "windows-4.0-386" ]];then
         echo "rename windows 386 $FILENAME"
-        mv $FILENAME clash.meta-windows-386-cgo.exe
+        mv $FILENAME lucky.core-windows-386-cgo.exe
     elif [[ $FILENAME =~ "windows-4.0-amd64" ]];then
         echo "rename windows amd64 $FILENAME"
-        mv $FILENAME clash.meta-windows-amd64-cgo.exe
-    elif [[ $FILENAME =~ "clash.meta-linux-arm-5" ]];then
-        echo "rename clash.meta-linux-arm-5 $FILENAME"
-        mv $FILENAME clash.meta-linux-armv5-cgo
-    elif [[ $FILENAME =~ "clash.meta-linux-arm-6" ]];then
-        echo "rename clash.meta-linux-arm-6 $FILENAME"
-        mv $FILENAME clash.meta-linux-armv6-cgo
-    elif [[ $FILENAME =~ "clash.meta-linux-arm-7" ]];then
-        echo "rename clash.meta-linux-arm-7 $FILENAME"
-        mv $FILENAME clash.meta-linux-armv7-cgo
+        mv $FILENAME lucky.core-windows-amd64-cgo.exe
+    elif [[ $FILENAME =~ "lucky.core-linux-arm-5" ]];then
+        echo "rename lucky.core-linux-arm-5 $FILENAME"
+        mv $FILENAME lucky.core-linux-armv5-cgo
+    elif [[ $FILENAME =~ "lucky.core-linux-arm-6" ]];then
+        echo "rename lucky.core-linux-arm-6 $FILENAME"
+        mv $FILENAME lucky.core-linux-armv6-cgo
+    elif [[ $FILENAME =~ "lucky.core-linux-arm-7" ]];then
+        echo "rename lucky.core-linux-arm-7 $FILENAME"
+        mv $FILENAME lucky.core-linux-armv7-cgo
     elif [[ $FILENAME =~ "linux" ]];then
         echo "rename linux $FILENAME"
         mv $FILENAME $FILENAME-cgo
